f4ddd1fa12602ee6330f8d919a774eef468cac8b
Matt Ha <matthewha1993@gmail.com>
2018-03-01 15:11:22 -0800

    Create main footer component

---
+++ src/components/MainFooter.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Component} from 'react';
+
+
+class MainFooter extends Component {
+
+  // ==================================================
+  // Initialize
+  // ==================================================
+
+  // ==================================================
+  // Callbacks
+  // ==================================================
+
+  // ==================================================
+  // Render
+  // ==================================================
+  render() {
+    return (
+      <div className="main-view-header">
+        <p>Footer</p>
+      </div>
+    );
+  }
+
+};
+
+export default MainFooter;
